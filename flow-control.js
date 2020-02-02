35af12d70b145dc13c213ee731f2dc8e99d04430
Gwendolyn Hill <gsmith8103@gmail.com>
2020-02-02 19:10:46 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,27 +1,34 @@
 function basicTeenager(age){
-  if (age >= 13 && age <= 19){
-  return "You are a teenager!";
+  if (age >=13 && age <=19){
+  return "You are a teenager!"
   }
 }
 
 function teenager(age) {
-  if (age >= 13 && age <= 19){
-  return "You are a teenager!";
+  if (age >=13 && age <=19){
+  return "You are a teenager!"
   } else {
-  return "You are not a teenager";}
+  return "You are not a teenager"}
 }
 
 function ageChecker(age) {
-  if (age >= 13 && age <= 19){
-return "You are a teenager!";
+  if (age >=13 && age <=19){
+return "You are a teenager!"
 } else if (age <=12) {
-return "You are a kid";
+return "You are a kid"
 } else {
-return "You are a grownup"}}
+return "You are a grownup"}
 }
 
 function ternaryTeenager(age) {
-  if (age >= 13 && age <= 19) {
+  if (age >=13 && age <=19) {
+return "You are a teenager"
+} else { (age >19 && age <13)
+return "You have an age"}
+}
+
+function switchAge(age) {
+if (age >= 13 && age <= 19) {
 return "You are a teenager";
 } else { (age > 19 && age < 13)
 return "You have an age";}
